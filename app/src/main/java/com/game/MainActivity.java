7fb0ad56286ac311268649b6c1990a72a06cbd4a
tom <tom@gmail.com>
2017-10-29 00:31:52 +0800

    Modify code.

--- app/src/main/java/com/game/MainActivity.java
+++ app/src/main/java/com/game/MainActivity.java
@@ -2,9 +2,12 @@ package com.game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtn;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
